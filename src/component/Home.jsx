6a674d26d5758d0a681268fc785902d4e157d66c
kjha4951 <128771901+kjha4951@users.noreply.github.com>
2024-08-21 11:34:34 +0530

    Update Home.jsx
    just add an new resume

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react'
-import pdf from '../pdf/Resume.pdf'
+import pdf from '../pdf/komal-jha-Resume2.pdf'
 import hero from '../data/hero.json'
 import Typed from 'typed.js'
 
