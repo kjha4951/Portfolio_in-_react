92e66ed0ba6d837305ffd2e9a70e661a9ed99b0c
kjha4951 <kjha4951@gmail.com>
2024-08-31 18:05:20 +0530

    remove transition contact

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -17,7 +17,9 @@ const Contact = () => {
     <>
     <div className="container contact" id='contact'>
       <h1>CONTACT ME</h1>
+      <div className="contact-icon"
     
+      >
         <a  href="https://www.instagram.com/unbeatable__girl/"  target='_blank'  className="items"><FaInstagram className='icons' />
         </a>
         <a href="mailto:kjha4951@gmail.com" target='_blank' className="items"> <SiGmail className='icons'/></a>
@@ -25,7 +27,7 @@ const Contact = () => {
         <a href="https://github.com/kjha4951"  target='_blank' className="items"> <FaGithubSquare className='icons'/></a>
         <a  href="https://www.linkedin.com/in/komal-jha-1a43a71b3/"  target='_blank' className="items"> <FaLinkedinIn className='icons'/></a>
       </div>
-   
+    </div>
     </>
   )
 }
