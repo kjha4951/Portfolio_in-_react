8db98a389bdc8dc4c68a277fadf7ad11f020dfeb
kjha4951 <kjha4951@gmail.com>
2024-08-31 18:03:50 +0530

    add index.css

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -17,10 +17,7 @@ const Contact = () => {
     <>
     <div className="container contact" id='contact'>
       <h1>CONTACT ME</h1>
-      <div className="contact-icon"
-      data-aos="zoom-in-up"
-            data-aos-duration="1000"
-      >
+    
         <a  href="https://www.instagram.com/unbeatable__girl/"  target='_blank'  className="items"><FaInstagram className='icons' />
         </a>
         <a href="mailto:kjha4951@gmail.com" target='_blank' className="items"> <SiGmail className='icons'/></a>
@@ -28,7 +25,7 @@ const Contact = () => {
         <a href="https://github.com/kjha4951"  target='_blank' className="items"> <FaGithubSquare className='icons'/></a>
         <a  href="https://www.linkedin.com/in/komal-jha-1a43a71b3/"  target='_blank' className="items"> <FaLinkedinIn className='icons'/></a>
       </div>
-    </div>
+   
     </>
   )
 }
