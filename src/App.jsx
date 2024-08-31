de8f466b693041ceae6dfc23c0be28c4e8277771
kjha4951 <kjha4951@gmail.com>
2024-08-31 18:29:04 +0530

    added css for the home

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   return (
    <>
    <Navbar/>
-   <div className="container">
+   <div className="container" style={{overflow:"hidden"}}>
    <Home/>
    <Education/>
    <Experience/>
