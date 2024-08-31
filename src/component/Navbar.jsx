5024ea596c2f77ecd8cf0cfaab76edf109a703dc
kjha4951 <kjha4951@gmail.com>
2024-08-31 18:44:18 +0530

    navbar sticky

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,7 +2,7 @@ import { Navbar, Container, Nav } from 'react-bootstrap'
 
 const MyNavbar = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar sticky="top"  collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
                 <Navbar.Brand href="#home">Portfolio</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
